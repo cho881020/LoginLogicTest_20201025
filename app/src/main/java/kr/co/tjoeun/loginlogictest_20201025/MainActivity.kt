ffa3d0834dd041f71b475990e6d37a2b0921a278
조경진 <kj_cho@idealidea.co.kr>
2020-10-25 12:48:10 +0900

    로그인 로직 흉내내기 마무리

--- app/src/main/java/kr/co/tjoeun/loginlogictest_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/loginlogictest_20201025/MainActivity.kt
@@ -2,10 +2,29 @@ package kr.co.tjoeun.loginlogictest_20201025
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+            if (inputEmail == "admin@test.com" && inputPw == "qwer") {
+                Toast.makeText(this, "관리자가 로그인했습니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
+
 }
